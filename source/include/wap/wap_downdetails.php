c04dbe754a2835a541743908e2a2b97101f54641
sailong <shailong123@126.com>
2013-08-08 15:31:23 +0800

    添加南山下载客户端页面

--- source/include/wap/wap_downdetails.php
+++ source/include/wap/wap_downdetails.php
@@ -10,7 +10,13 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
-
+$width=$_G['gp_pic_width'];
+if($width)
+{
+	$width="width='{$width}'";
+}else{
+	$width="width='720'";
+}
 
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 if(strpos($userAgent,"iPhone") || strpos($userAgent,"iPad") || strpos($userAgent,"iPod") || strpos($userAgent,"iOS"))
@@ -25,31 +31,9 @@ else if(strpos($userAgent,"Android"))
 	$dz_down_url = DB::result_first("select app_version_file from tbl_app_version where app_version_type='android' and field_uid=0 order by app_version_addtime desc limit 1 ");
 	$mlh_down_url = DB::result_first("select app_version_file from tbl_app_version where app_version_type='android' and field_uid=1186 order by app_version_addtime desc limit 1 ");
 	$ns_down_url = DB::result_first("select app_version_file from tbl_app_version where app_version_type='android' and field_uid=1160 order by app_version_addtime desc limit 1 ");
-	
 }
 
-
-/*定义 需要查询用户组下的ID*/
-$group_ids = array(
-                   '20'=>'资讯',
-				   '21'=>'球场',
-				   //'22'=>'球具',
-				   //'23'=>'教学',
-				   //'24'=>'球星',
-				   '25'=>'赛事',
-				   '26'=>'旅游',
-				   '20'=>'品牌俱乐部',
-				   '1889180'=>'手机报',
-				   '1889013'=>'资讯'
-				   );
-
-$nva_static_url="http://wap.bwvip.com/index.php?mod=news&ac=group_list&groupid=";
-
-$num =10; $where = '';
-
-/*获取相对 频道下的 或者是某个UId下的 blogs */
-$get_groupid = getgpc('groupid');
-$get_groupid = empty($get_groupid) ? getgpc('uid') : $get_groupid;
+/*球场uid----field_uid*/
 $field_uid = empty($_G['gp_field_uid']) ? 0 : $_G['gp_field_uid'];
 
 
@@ -63,46 +47,17 @@ switch ($field_uid)
       $top_pic = "<img src='http://www.bwvip.com/images/wap/dazheng-top.png' />";
       break;
     case 1186:
-	  $top_pic = "<a href='{$mlh_down_url}'><img src='http://www.bwvip.com/images/wap/meilanhu-top.png' /></a>";
+	  $top_pic = "<a href='{$mlh_down_url}'><img src='http://www.bwvip.com/images/wap/meilanhu-top.png'/></a>";
       break;
 	case 1160:
 	  $top_pic = "<a href='{$ns_down_url}'><img src='http://www.bwvip.com/images/wap/nanshan-top.png' /></a>";
       break;
     default:
-      $top_pic = "<img src='http://www.bwvip.com/images/wap/dazheng-top.png' />";
+      $top_pic = 'dazheng-top.png';
       break;
 }
 
-if($_G['gp_uid']){
-	$where = " and uid = '".getgpc('uid')."'";
-}
-if($_G['gp_id']){
-	$where .= " and blogid>{$_G['gp_id']} ";
-}
-$num =3;
-$uid = getgpc('uid');
-
-/*最新博客*/
-
-$new_blogs_query = DB::query(" select `blogid`,`subject` from ".DB::table('home_blog')." where 1=1 ".$where." limit ".$num);
-
-
-while($new_blogs_result = DB::fetch($new_blogs_query)){
-	$new_blogs_list[$new_blogs_result['blogid']]= $new_blogs_result['subject'];
-}
-
-
-$id = getgpc('id');
-
-$news_blog = DB::fetch_first(" SELECT b.`subject`,b.`dateline`,bf.`tag`,bf.`message`,bf.`pic` FROM ".DB::table('home_blog')." as b LEFT join ".DB::table('home_blogfield')." as bf ON b.blogid=bf.blogid where b.blogid='".$id."'");
-
-
-$aa=str_replace("src=\"/Public/editor/attached","src=\"http://www.bwvip.com/Public/editor/attached",$news_blog['message']);
-$aa=str_replace("src=\"data/attachment/","src=\"http://www.bwvip.com/data/attachment/",$aa);
-$news_blog['message']=$aa;
-
-
-include_once template("wap/news_details");
+include_once template("wap/down_details");
 
 
 ?>
---
+++ source/module/wap/wap_down.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: home_space.php 22839 2011-05-25 08:05:18Z monkey $
+ */
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$uid = empty($_GET['uid']) ? 0 : intval($_GET['uid']);
+
+if($_GET['username']) {
+	$member = DB::fetch_first("SELECT uid FROM ".DB::table('common_member')." WHERE username='$_GET[username]' LIMIT 1");
+	if(empty($member)) {
+		showmessage('space_does_not_exist');
+	}
+	$uid = $member['uid'];
+}
+
+$dos = array('downdetails');
+
+$do = (!empty($_GET['do']) && in_array($_GET['do'], $dos))?$_GET['do']:'index';
+
+$is_exist_amp = is_int(strpos($url_this,'&amp;'));
+if($is_exist_amp) {
+	$url_this =  "http://".$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	$url_this = str_replace("&amp;","&",$url_this);
+	header("Location:{$url_this}");exit;
+}
+if($_GET['test'] == 1) {
+	echo '<pre>'; 
+	$url_this =  "http://".$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	$is_exist = is_int(strpos($url_this,'&&'));
+	var_dump($is_exist);
+	$url_this=str_replace("","&amp;",$news_blog['message']);
+	var_dump($_SERVER);
+	echo $url_this;die;
+    var_dump(libfile('wap/'.$do, 'include'));
+}
+require_once libfile('wap/'.$do, 'include');
+
+?>
