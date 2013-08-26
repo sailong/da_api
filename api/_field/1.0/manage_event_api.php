fc8317d43f2067ff6738f9491d29535605c6a310
sailong <shailong123@126.com>
2013-08-26 10:27:22 +0800

    修改生成二维码方法 修改wap跳转页链接

--- api/_field/1.0/manage_event_api.php
+++ api/_field/1.0/manage_event_api.php
@@ -801,29 +801,35 @@ if($ac == 'uidtomobile')
 }
 
 //生成二维码成功返回路径，失败返回 false
-function erweima($phone)
+function erweima()
 {
 	$phone = mt_rand(1000000000,9999999999);
     //如果没有就生成二维码
-	include "../tool/phpqrcode/qrlib.php";
-	$prefix = "..";
+	$path_erweima_core = dirname(dirname(dirname(dirname(__FILE__))));
+	
+	include $path_erweima_core."/tool/phpqrcode/qrlib.php";
+	$prefix = $path_erweima_core;
 	$save_path="/upload/erweima/";
 	$now_date = date("Ymd",time());
-	$full_save_path=$save_path.$now_date."/";
+	$full_save_path=$path_erweima_core.$save_path.$now_date."/";
+
 	if(!file_exists($prefix.$save_path))
 	{
 		mkdir($prefix.$save_path);
 	}
-	if(!file_exists($prefix.$full_save_path))
+	if(!file_exists($full_save_path))
 	{
-		mkdir($prefix.$full_save_path);
+		$a = mkdir($full_save_path);
 	}
-	$pic_filename=$prefix.$full_save_path.$phone.".png";
+	
+	$pic_filename=$full_save_path.$phone.".png";
 	$sql_save_path = $full_save_path.$phone.".png";
 	$errorCorrectionLevel = "L";
 	$matrixPointSize=9;
 	$margin=1;
+	
 	QRcode::png($phone, $pic_filename, $errorCorrectionLevel, $matrixPointSize, $margin); 
+	
 	if(file_exists($pic_filename))
 	{
 		return $sql_save_path;
