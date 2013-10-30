41304058152203dc0bcdfb2a89c62f33206a4d35
sailong <shailong123@126.com>
2013-10-30 15:31:03 +0800

    添加球场客户端管理

---
+++ dazheng/Lib/Model/field_manageModel.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *    #Case		tankuang
+ *    #Page		userModel.class.php (用户)
+ *
+ *    @author		Jack
+ *    @E-mail		68779953@qq.com
+ */
+class field_manageModel extends Model{
+
+	//list and page
+	function field_list_pro($bigwhere="", $page_size=20, $sort="field_addtime desc") 
+	{
+		$page = intval(get("p"))?get("p"):1;
+
+		$where = " 1 ";
+
+		if(get("starttime")!="")
+		{
+			$where .=" and field_addtime>=".strtotime(get("starttime"))." ";
+		}
+		if(get("k")!="")
+		{
+			$where .=" and field_name like '%".get("k")."%' ";
+		}
+		if(get("endtime")!="")
+		{
+			$endtime=strtotime(get("endtime"))+24*3600;;
+			$where .=" and field_addtime<=$endtime ";
+		}
+
+ //联表查询信息
+		$data["item"]=M("field")->where($where.$bigwhere)->order($sort)->page($page.",".$page_size)->select();
+		$data["total"]=M("field")->where($where.$bigwhere)->count();
+		
+		import ("@.ORG.Page");
+		$page = new page ($data["total"], $page_size );
+		$data["pages"] = $page->show();
+
+		return $data;
+	}
+
+
+	//nopage select limit 
+	function field_select_pro($bigwhere="",$limit=999999, $sort=" field_addtime desc ") 
+	{
+		
+		$where = " 1 ";
+
+		$data["item"]=M("field")->where($where.$bigwhere)->order($sort)->limit($limit)->select();
+		$data["total"]=M("field")->where($where.$bigwhere)->count();
+
+		return $data;
+	}
+
+
+	
+	
+
+}
+?>
